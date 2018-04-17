2bd4c87b935decac96b13654edb078aeebf90f6f
Lamgor <513968746@qq.com>
2018-04-16 21:57:17 -0500

    fix #22实现个人信息及简历管理 (#23)
    * fix #13实现公司和个人注册
    
    * add marco.html
    
    * 修改一些文件
    
    * fix #22 实现个人信息及简历管理

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,7 +1,10 @@
-from flask.ext.wtf import Form
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
+import os
+from flask import url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError,IntegerField
 from wtforms.validators import Required,Length,Email,EqualTo
-from wtforms import ValidationError
+from jobplus.models import db,User
+from flask_wtf.file import FileField,FileRequired
 
 class LoginForm(Form):        #登录页面的内容  
     email = StringField('邮箱',validators=[Required(),Email()])
@@ -9,39 +12,66 @@ class LoginForm(Form):        #登录页面的内容
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
 
-#    def validate_email(self,field):
-#        if field.data and not User.query.filter_by(email=field.data).first()
-#            raise ValidationError('')
-#还差检测密码的，暂时看不懂怎么写
+    def validate_email(self,field):
+        if field.data and not User.query.filter_by(email=field.data).first()
+            raise ValidationError('email has not register')
+
+    def validate_password(self,field):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and not user.check_password(field.data):
+            raise ValidationError('passwrod error')
 
 
-class PersonalRegister(Form):    #求职者注册页面内容
+class Register(Form):    #求职者和公司的注册页面内容
     username = StringField('用户名',validators=[Required(),Length(1,64)])
     email = StringField('邮箱',validators=[Required(),Email()])
     password = PasswordField('密码',validators=[Required(),Length(6,24)])
     repear_password = PasswordField('重复密码',validators=[Requried(),Length(6,24),EqualTo('password',message = '密码要一致')])
     submit = SubmitField('注册')
 
-#等数据库的名称确定好才用，这里默认数据库的用户表叫user
-#    def validate_email(self,field):
-#        if User.query.filter_by(email=field.data).first()
-#            raise ValidationError('邮箱已经被注册')
-#
-#    def validate_username(self,field):
-#        if User.query.filter_by(username=field.data).first()
-#            raise ValidationError('用户名已经被注册')
 
-class CompanyRegister(Form):
-    companyname = StringField('公司名称',validators=[Required(),Length(1,64)])
-    email = StringField('邮箱',validators=[Required(),Email()])
-    password = PasswordField('密码',validators=[Required(),Length(6,24)])
-    repeat_password = PasswordField('重复密码',validators=[Required(),Length(6,24),EqualTo('password',message = '密码要一致')])
-    submit = SubmitField('注册')
+    def validate_email(self,field):        
+        if User.query.filter_by(email=field.data).first()
+            raise ValidationError('邮箱已经被注册')
 
-#    def validate_email(self,field):
-#        if Company.query.filter_by(email=field.data).first()
-#            raise ValidationError('邮箱已经被注册')
-#
-#    def validate_companyname(self,field):
-#        if Company.query.filter_by(companyname=field.data).first()
-#            raise ValidationError('公司名称已经被注册')
+    def validate_name(self,field):
+        if User.query.filter_by(name=field.data).first()
+            raise ValidationError('名已经被注册')
+
+    def create_user(self):
+        user = User(name=self.name.data,email=self.email.data,password=self.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+class UserProfileForm(Form):
+    real_name = StringField('name',validators=[Required(),Length(1,64)])
+    email = StringField('email',validators=[Required(),Email()])
+    password = PasswordField('password(no write no change)')
+    phone = StringField('phone number',validators=[Required()])
+    work_year = IntegerField('work year')
+    resume_url = FileField('upload resume')
+    submit = SubmitField('submit')
+
+    def validate_phone(self,field):
+        phone = field.data
+        if phone[:2] not in ('13','15','18') and len(phone) !=11:
+            raise ValidationError('please enter a correct phone number')
+
+    def upload_resume(self):
+        f = self.resume.data
+        filename = self.real_name.data + '.pdf'
+        f.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),'static','resume',filename))
+        return filename
+
+    def update_profile(self,user):
+        user.real_name = self.real_name.data
+        user.email = self.email.data
+        if self.password.data:
+            user.password = self.password.data
+        user.phone = self.phone.data
+        user.work_year = self.work_year.data
+        filename = self.upload_resume()
+        user.resume_url = url_for('static',filename=os.path.join('resumes',filename))
+        db.session.add(user)
+        db.session.commit()
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,43 +1,57 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,Blueprint
 from flask_login import login_user,logout_user,login_required
 from flask_wtf import Form
-from jobplus.forms import LoginForm
+from jobplus.forms import LoginForm,RegisterForm
+from jobplus.forms import User,db,Job
 
-@front.route('/login',methods=['GET','POST'])    
+front = Blueprint('front',__name__)
+
+#Home页设计还没完成
+@front.route('/')
+def index():
+    return render_template('index.html')
+
+
+@front.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm
     if form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()  #数据库表名字User，可以改。这里还缺少判断登录人员的类型。
-        login_user(user,form.remember_me.data)
-        return redirect(url_for('用户简历管理'))
+        user = User.query.filter_by(email = form.email.data).first()
+        if user.is_disable:
+            flash('user had been banned')
+            return redierct(url_for('front.login'))
+        #判断用户角色,在models有定义
+        else:
+            login_user(user,form.remember_me.data)
+            next = 'user.profile'
+            if user.is_admin:
+                next = 'admin.index'
+            elif user.is_company:
+                next = 'company.profile'
+            return redirect(url_for('next'))
     return render_template('login.html',form=form)
 
 @front.route('/logout')
 @login_required    #若没有登录则不能浏览
 def logout():
     logout_user()
-    return redirect(url_for('登出到首页？index'))
+    return redirect(url_for('front.index'))   #登出回到Home页
 
 
 @front.route('/PersonalRegister',methods=['GET','POST'])
 def PersonalRegister():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data,
-                username=form.username.data,
-                password=form.password.data)
-        db.session.add(user)    #提交到user表
-        db.session.commit()
+        form.create_user()   #create_user()函数包含session.add()
         return redirect(url_for('front.login'))
-    return render_template('PersonalRegister.html',form=f
+    return render_template('PersonalRegister.html',form=form)
             
 @front.route('/CompanyRegister',methods=['GET','POST'])
 def CompanyRegister():
     form = RegistrationForm()
     if form.validate_on_submit():
-        company = Company(email=form.email.data,
-            username=form.username.data,
-            password=form.password.data)    #Company表单信息
+        company = form.create_user()
+        company.role = User.ROLE_COMPANY    #设置成公司用户
         db.session.add(company)    #提交到Company表
         db.session.commit()
         return redirect(url_for('front.login'))
